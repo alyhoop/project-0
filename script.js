bc64fb67d60c5048331004b59e4bd0ab2606ce49
Alyssa Hooper <nesteralyssa@gmail.com>
2020-04-17 12:41:05 -0700

    form box

--- script.js
+++ script.js
@@ -28,3 +28,60 @@ function totalReset() {
   document.getElementById('item-count').textContent = 0;
   document.getElementById('unchecked-count').textContent = 0;
 }
+
+var data = {
+	listItems: []
+};
+
+var template = function () {
+
+	// If there are no list items
+	if (data.listItems.length < 1) return '<p><em>You do not have any list items yet. Try adding one with the form above.</em></p>';
+
+	// If there are
+	return '<ul>' + data.listItems.map(function (item) {
+		return '<li>' + item + '</li>';
+	}).join('') + '</ul>';
+
+};
+
+var render = function () {
+	var list = document.querySelector('#list');
+	if (!list) return;
+	list.innerHTML = template();
+};
+
+
+/*!
+ * Sanitize and encode all HTML in a user-submitted string
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {String} str  The user-submitted string
+ * @return {String} str  The sanitized string
+ */
+var sanitizeHTML = function (str) {
+	var temp = document.createElement('div');
+	temp.textContent = str;
+	return temp.innerHTML;
+};
+
+document.addEventListener('submit', function (event) {
+
+	// Make sure the submitted form was for our list items
+	if (!event.target.matches('#add-to-list')) return;
+
+	// Stop the form from submitting
+	event.preventDefault();
+
+	// Get the list item
+	var item = event.target.querySelector('#list-item');
+	if (!item || item.length < 1) return;
+
+	// Update the data and UI
+	data.listItems.push(sanitizeHTML(item.value));
+	render();
+
+	// Clear the field and return to focus
+	item.value = '';
+	item.focus();
+
+}, false);
